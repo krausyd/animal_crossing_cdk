b6aece269fd1e37d67ff32577923758c276dbad8
karina <kguerrerofernandez@instructors.2u.com>
2024-09-21 17:18:46 -0700

    Add code generated by running `npx cdk init app`

---
+++ lib/app.ts
@@ -0,0 +1,31 @@
+/*
+ * Let's define the infra I need
+1. AppConfig:
+ a) Resources
+  i. Fish (P0)
+  ii. Bugs (P0)
+  iii. Sea creatures (P0)
+  iv. Art (P0)
+ b) Recipes (P1) 
+ c) Villagers (P1)
+ d) Flowers (P2)
+2. DynamoDB
+ a) Users+Passwords (P0)
+ b) Islands (P0) - Limit to 1 initially, P1 allow more than 1
+ c) Fish (P0)
+ d) Bugs (P0)
+ e) Sea creatures (P0)
+ f) Songs (P1)
+ g) Recipes (P1)
+ h) Villagers (P1)
+ i) Turnip history (P2)
+ j) Visitors history (P3)
+ k) Weather history (P3 (for celeste maybe))
+3. Route53 (P0)
+4. ECS (P0) (Or whatever is needed for Python backend NodeJs)
+5. Cloudfront (P0) (Or whatever is needed for React front end)
+6. Cloudformation (P0) (Metrics, alarms and all that)
+7. SNS (P1) (For SMS notification on when you should be fishing + catching bugs)
+and more...
+ */
+
